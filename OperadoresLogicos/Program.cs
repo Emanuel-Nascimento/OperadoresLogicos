2152ccda5a1c7e3d83f72e7a9f948d9036b3c522
GlobalEmanuel <global.emanuel@ao3tech.com>
2022-01-02 19:26:32 -0300

    Adicionar arquivos de projeto.

---
+++ OperadoresLogicos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores Lógicos AND [&&] e OR [ || ]
+
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || true);
+            Console.WriteLine(true || true);
+            Console.WriteLine(false || false);
+
+           
+
+            Console.WriteLine(true && false);
+            Console.WriteLine(false && true);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            Console.ReadLine();
+
+        }
+    }
+}
